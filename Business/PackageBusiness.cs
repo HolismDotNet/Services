3fe4ca3438cdddd34403475bc5bcda6f9b7c8c8f
Saeed Nemati <sn.1361@gmail.com>
2022-01-22 17:24:31 +0330

    committed by script

--- Business/PackageBusiness.cs
+++ Business/PackageBusiness.cs
@@ -1,1 +1,10 @@
-namespace Holism.Package.Business;
+using Holism.Services.DataAccess;
+
+namespace Holism.Services.Business;
+
+public class PackageBusiness : Business<Package, Package>
+{
+    protected override ReadRepository<Package> ReadRepository => Repository.Package;
+
+    protected override Repository<Package> WriteRepository => Repository.Package;
+}
